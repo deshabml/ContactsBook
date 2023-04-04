ef45b892b82ad1160c26a649865d91c6a7e63ffd
deshabml <deshab@yandex.ru>
2023-04-04 23:37:26 +0300

    Start Project

---
+++ ContacsBook/ContacsView.swift
@@ -0,0 +1,20 @@
+//
+//  ContacsView.swift
+//  ContacsBook
+//
+//  Created by Лаборатория on 03.04.2023.
+//
+
+import UIKit
+
+class ContacsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
